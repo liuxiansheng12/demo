3eaa4bf224dc77cab5529c5ca2087cd5e6e5732f
liushanzhuang <liushanzhuang@metrodata.cn>
2021-04-21 10:22:56 +0800

    提交

--- static/chongmingming/index.js
+++ static/chongmingming/index.js
@@ -1,11 +1,26 @@
 
 console.log("呵呵呵 dsas");
 
+console.log("呵呵呵 测试");
+// console.log("呵呵呵 sssss");
+
+
+
+
+console.log("呵呵呵 测试");
 
 console.log("呵呵呵 sssss");
 
+function a() {
+
+}
+
+function b() {
+
+}
+
 
 console.log("呵呵呵 sssss");
-
 
 console.log("呵呵呵 ssdssssc");
+
